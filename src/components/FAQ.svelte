7b9c96fa210100566c8b81aa6019748e0294c3fe
Davi Santos <brisolo32@outlook.com>
2022-10-26 16:35:53 -0300

    Added faq, prob going to change the entire css tho

--- src/components/FAQ.svelte
+++ src/components/FAQ.svelte
@@ -1,15 +1,27 @@
 <div class="faq-page border bg">
     <h2>FAQ (Frequently asked questions)</h2>
     <div class="faq-time">
-        <h2>What is this?:</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab aliquid, molestiae, eum quo dolores error iure dolorum maxime expedita accusantium officiis, necessitatibus quaerat. Alias pariatur omnis quibusdam debitis voluptatibus?</p>
+        <h2>
+            What is Project Black Pearl?
+        </h2>
+        <p>
+            • Project Black Pearl (PBP) is an open-source project founded by Nebula which aims to create a Steam-like app for pirated games.
+        </p>
     </div>
     <div class="faq-time">
-        <h2>How can i help?:</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab aliquid, molestiae, eum quo dolores error iure dolorum maxime expedita accusantium officiis, necessitatibus quaerat. Alias pariatur omnis quibusdam debitis voluptatibus?</p>
+        <h2>
+            How can I help with the development of PBP?
+        </h2>
+        <p>
+            • First off, join the discord server on there create a ticket under "Development Help" in "#support" and state which aspects of PBP you would like to help develop, which programming languages you have experience with, and a link to your GitHub profile if you have one.
+        </p>
     </div>
     <div class="faq-time">
-        <h2>Is there an ETA?:</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab aliquid, molestiae, eum quo dolores error iure dolorum maxime expedita accusantium officiis, necessitatibus quaerat. Alias pariatur omnis quibusdam debitis voluptatibus?</p>
+        <h2>
+            What if I have a question that wasn't covered here?
+        </h2>
+        <p>
+            • Please either create a ticket under "Questions!" in "#support" or ping/DM Nebula or any other Lead Developers and state your question. <a href="https://dontasktoask.com/">Do not ask to ask</a>
+        </p>
     </div>
 </div>
