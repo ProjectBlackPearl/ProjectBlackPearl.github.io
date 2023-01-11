054f93ad16f7ac9d6cbc0af6d89cd0547bc0b26d
Davi Santos <brisolo32@outlook.com>
2023-01-11 16:03:43 -0300

    Edited download url and added wiki url

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,37 @@
-<svelte:head>
-  <script src="https://kit.fontawesome.com/dacbc752b2.js" crossorigin="anonymous"></script>
-</svelte:head>
-
 <script lang="ts">
-  import MainText from './components/MainText.svelte'
+  import MainText from "./components/MainText.svelte";
 </script>
 
-<main> 
+<svelte:head>
+  <script
+    src="https://kit.fontawesome.com/dacbc752b2.js"
+    crossorigin="anonymous"
+  ></script>
+</svelte:head>
+
+<main>
   <!-- Creates a ul element with anchors linking to different links -->
   <ul class="navbar">
     <!-- Logo -->
     <li class="navbar-items">
-       <img src="https://cdn.discordapp.com/attachments/922635062602788884/1035922289180557382/Logo.png" alt="Logo" width="250px">
+      <img src="Logo.png" alt="Logo" width="250px" />
     </li>
-    <!-- Download URL (WIP) -->
+    <!-- Download URL -->
     <li class="navbar-items" style="float:right">
-      <a href="https://github.com/ProjectBlackPearl/ProjectBlackPearl">
-        Download
-      </a>
+      <a href="https://github.com/ProjectBlackPearl/pbpl"> Download </a>
     </li>
     <!-- Source code -->
     <li class="navbar-items" style="float:right">
-      <a href="https://github.com/ProjectBlackPearl">
-        Source code
-      </a>
+      <a href="https://github.com/ProjectBlackPearl"> Source code </a>
+    </li>
+
+    <li class="navbar-items" style="float:right">
+      <a href="https://projectblackpearl.github.io/wiki"> Wiki </a>
     </li>
     <!-- Join the community (aka: Discord Server) -->
     <li class="navbar-items" style="float:right">
-      <a href="https://discord.gg/PbM5AjamNH">
-        Join the community
-      </a>
+      <a href="https://discord.gg/PbM5AjamNH"> Join the community </a>
     </li>
   </ul>
-  <MainText/>
+  <MainText />
 </main>
